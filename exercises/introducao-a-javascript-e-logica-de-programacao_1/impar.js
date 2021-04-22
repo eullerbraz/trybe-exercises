88638a2dfeb460bcac2c4a10534f410654bb02fa
Euller Braz <euller.braz99@gmail.com>
2021-04-22 19:10:06 -0300

    Resolvido exercicios impar e lucro

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/impar.js
@@ -0,0 +1,12 @@
+let n1 = 2;
+let n2 = 8;
+let n3 = 6;
+let resultado;
+
+if ((n1 % 2 == 1) || (n2 % 2 == 1) || (n3 % 2 == 1)) {
+    resultado = true;
+} else {
+    resultado = false;
+}
+
+console.log(resultado);
