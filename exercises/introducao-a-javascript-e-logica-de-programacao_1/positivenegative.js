d7a8112d08764403ee708d872737b8b02112ce32
Euller Braz <euller.braz99@gmail.com>
2021-04-22 18:26:32 -0300

    Resolvido exercicios maior e positive

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/positivenegative.js
@@ -0,0 +1,12 @@
+let a = 0;
+let resultado;
+
+if (a>0) {
+   resultado = "positive"; 
+} else if (a<0) {
+    resultado = "negative";
+} else {
+    resultado = "zero";
+}
+
+console.log(resultado);
