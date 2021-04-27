be881e6896f36af6b44a94ea3efcda8088e2bd44
Euller Braz <euller.braz99@gmail.com>
2021-04-27 18:24:57 -0300

    Resolvido exercicio 2 e 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/parte2ex2.js
@@ -0,0 +1,14 @@
+function indexMaior(array) {
+    let maior = array[0];
+    let pos = 0;
+    for (let index in array) {
+        if (array[index] > maior) {
+            maior = array[index];
+            pos = index;
+        }
+    }
+    return pos;
+}
+
+let arrayTest = [2, 3, 6, 7, 10, 1];
+console.log(indexMaior(arrayTest));
