f59d5e05dbc2cfbefea1e481cbe0ec83c62add7b
Euller Braz <euller.braz99@gmail.com>
2021-05-04 12:38:14 -0300

    Resolvido exercicio 10

--- exercises/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
@@ -191,3 +191,24 @@ function selectedTasks() {
 }
 selectedTasks();
 
+//Exercicio 10
+function dayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  let defaultColor = 'rgb(119,119,119)';
+
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+}
+dayColor();
+
+//Bonus
