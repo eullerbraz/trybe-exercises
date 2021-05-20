e22db0f53c99905bf1ae6ef0ee11adc8c2745aee
Euller Braz <euller.braz99@gmail.com>
2021-05-20 14:46:08 -0300

    Resolvido exercicios aula

---
+++ exerciciosAula/7_2/exercise4.js
@@ -0,0 +1,32 @@
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const clone = Object.assign(person, lastName);
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+clone.name = 'Maria';
+
+console.log('Mudando a propriedade name através do objeto clone')
+console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log('--------------');
+
+person.lastName = 'Ferreira';
+
+console.log('Mudando a propriedade lastName através do objeto person');
+console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+
+const newPerson = Object.assign({},person);
+newPerson.name = 'Gilberto';
+
+console.log('Mudando a propriedade name através do objeto newPerson');
+console.log(newPerson);
+console.log(person);
