cc60ce56e786b4837a0c00dd2a52893bc703b06a
Euller Braz <euller.braz99@gmail.com>
2021-05-28 15:57:12 -0300

    Resolvido Exercicio Reduce-Map

---
+++ exerciciosAula/8_4/exercise2.js
@@ -0,0 +1,97 @@
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+//Função
+const betterSubjects = (estudantes) => {
+  return estudantes.map((estudante) => {
+    const betterScore = estudante.materias.reduce((result, subject) => {
+      return ((result.nota > subject.nota) ? result : subject);
+    }).name;
+    return {name: estudante.nome, materia: betterScore}
+  })
+}
+
+//Teste
+const assert = require('assert');
+const expected = [
+  { name: 'Jorge', materia: 'Português' },
+  { name: 'Mario', materia: 'Biologia' },
+  { name: 'Jorge', materia: 'Português' },
+  { name: 'Maria', materia: 'Química' },
+  { name: 'Natalia', materia: 'Português' },
+  { name: 'Wilson', materia: 'Português' },
+];
+
+assert.deepStrictEqual(betterSubjects(estudantes), expected);
