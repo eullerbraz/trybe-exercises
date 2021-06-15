6a8ad8aa5d7fc29d9c28b1be4020e969df8a325e
Euller Braz <euller.braz99@gmail.com>
2021-06-15 19:33:07 -0300

    Resolvido Exercicio 6

---
+++ exercises/bloco_10/testes-automatizados-com-jest_2/exercise6.test.js
@@ -0,0 +1,39 @@
+const { getAnimal, getAnimalByAge } = require('./exercise6.js');
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
+
+//6.2
+describe('Testando promise - findAnimalByAge', () => {
+  describe('Quando existe o animal com o a idade', () => {
+    test('Verifique o primeiro nome do animal no array retornado', () => {
+      expect.assertions(1);
+      const animals = [{ name: 'Preguiça', age: 5, type: 'Cat' }];
+      return expect(getAnimalByAge(5)).resolves.toEqual(animals);
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return expect(getAnimalByAge(3)).rejects.toBe('Nenhum animal encontrado!');
+    });
+  });
+});
