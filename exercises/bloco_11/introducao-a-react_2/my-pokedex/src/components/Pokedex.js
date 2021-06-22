753f10a0d1716d484469ed2aaad69fd13d4bad2e
Euller Braz <euller.braz99@gmail.com>
2021-06-22 19:17:04 -0300

    Adicionado PropTypes

--- exercises/bloco_11/introducao-a-react_2/my-pokedex/src/components/Pokedex.js
+++ exercises/bloco_11/introducao-a-react_2/my-pokedex/src/components/Pokedex.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Pokemon from './Pokemon';
 import pokemons from '../data';
 import '../Pokedex.css';
+import PropTypes from 'prop-types';
 
 class Pokedex extends React.Component {
   render() {
@@ -15,5 +16,17 @@ class Pokedex extends React.Component {
     );
   }
 }
+
+Pokemon.propTypes = {
+  pokeInfo: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      measurementUnit: PropTypes.string,
+      value: PropTypes.number
+    }),
+    image: PropTypes.string,
+  }).isRequired,
+};
 
 export default Pokedex;
