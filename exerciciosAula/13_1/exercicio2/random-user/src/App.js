696ae04799e6663ecbe4a8141d5b3eca0a0fedd2
Euller Braz <euller.braz99@gmail.com>
2021-07-02 12:51:01 -0300

    Exemplos Aula

---
+++ exerciciosAula/13_1/exercicio2/random-user/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PersonDetails from './components/PersonDetails';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <PersonDetails />
+    );
+  }
+}
+
+export default App;
